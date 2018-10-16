f143d039b24c0c4cf5578b2ac1d93cc18b90cba6
Kristian Weegink <k.weegink@gmail.com>
2018-10-16 22:10:21 +1000

    correct updated file

--- clock/sketch.js
+++ clock/sketch.js
@@ -10,7 +10,6 @@ function setup() {
 }
 
 function draw() {
-    push();
     background(0);
     translate(width/2, height/2);
 
@@ -44,18 +43,17 @@ function draw() {
     var y3 = 0.5*sin(PI - 120/180*PI);
     var c0 = map(angle, 0, 2*PI, 0, 1);
     
-    noStroke;
+    noStroke();
     h_s = map(r_s, 0, R, 0, 1);
     h_s = (h_s + c0)%1;
     fill(h_s,0.2,0.8);
     triangle(r_s*x1, r_s*y1, r_s*x2, r_s*y2, r_s*x3, r_s*y3);
     noFill();
     strokeWeight(4);
-    h_m = map(r_s, 0, R, 0, 1);
+    h_m = map(r_m, 0, R, 0, 1);
     h_m = (h_m + c0)%1;
     stroke(h_m,0.2,0.8);
     triangle(r_m*x1, r_m*y1, r_m*x2, r_m*y2, r_m*x3, r_m*y3);
     stroke(c0,0.2,0.8);
     triangle(R*x1, R*y1, R*x2, R*y2, R*x3, R*y3);
-    pop();
 }
