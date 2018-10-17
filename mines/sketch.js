865808e1ed7da14ebe3a07490b508f7cea0f607e
Kristian Weegink <k.weegink@gmail.com>
2018-10-17 12:39:49 +1000

    Added game over and mass clearing 0s

--- mines/sketch.js
+++ mines/sketch.js
@@ -6,12 +6,14 @@ var mine_field = new Array((w+2)*(h+2));
 var cleared = new Array((w+2)*(h+2));
 var flag = new Array((w+2)*(h+2));
 var mine_count = new Array((w+2)*(h+2));
-var playing = true;
+var win = false;
+var lose = false;
+var endText = 'Cleared';
 
 function setup() {
     createCanvas(width, height);
     mine_count.fill(0);
-    cleared.fill(0);
+    cleared.fill(false);
     flag.fill(false);
 
     for (let i=0; i<w; i++) {
@@ -38,23 +40,40 @@ function mouseClicked() {
         if (keyIsPressed && keyCode == CONTROL ) {
             flag[k] = true;
         } else {
-            playing = !mine_field[k];
-            cleared[k] = 1;
+            lose = mine_field[k];
+            toClear = [k];
+            while(toClear.length > 0) {
+                let i = toClear.pop();
+                cleared[i] = true;
+                if (mine_count[i]==0) {
+                    for (let n = -1; n <= 1; n++) {
+                        for (let m = -1; m <= 1; m++) {
+                            let j = i + n + m*(w+2);
+                            if (!cleared[j]) {
+                                toClear.push(j);
+                            }
+                        }
+                    }
+                   
+                }
+            }
         }
     }
 }
 
-function gameOver() {
+function gameOver(good) {
+        endText = good?'Cleared':'KABOOOM!!';
         noStroke();
         fill('blue');
         textAlign(CENTER);
         textSize(64);
-        text("KABOOOOM", width/2, height/2);
+        text(endText, width/2, height/2);
 }
 
 function draw() {
     background(150);
     stroke(0);
+    win = true;
     for (let i=0; i<w; i++) {
         for (let j=0; j<h; j++) {
             let k = (i+1)+(j+1)*(w+2);
@@ -71,20 +90,36 @@ function draw() {
                 }
             }
             rect(i*width/w, j*height/h, width/w, height/h);
-            if (cleared[k]==1 && !(mine_field[k])) {
-                push()
-                noStroke();
-                fill(0);
-                textSize(20);
-                textAlign(CENTER, CENTER);
-                text(mine_count[k], (i+0.5)*width/w, (j+0.5)*height/h);
-                pop(); 
+            if (!mine_field[k]) {
+                win &= cleared[k];
+                if (cleared[k]) {
+                    push()
+                    noStroke();
+                    fill(0);
+                    textSize(20);
+                    textAlign(CENTER, CENTER);
+                    text(mine_count[k], (i+0.5)*width/w, (j+0.5)*height/h);
+                    pop(); 
+                }
             }
             pop();
         }
     }
-    if (!playing) {
-        gameOver();
+    if (win || lose) {
+        for (let i=0; i<w; i++) {
+            for (let j=0; j<h; j++) {
+                let k = (i+1)+(j+1)*(w+2);
+                if (mine_field[k]) {
+                    push()
+                    noStroke();
+                    fill(0);
+                    ellipseMode(RADIUS);
+                    ellipse((i+0.5)*width/w, (j+0.5)*height/h, 20);
+                    pop(); 
+                }
+            }
+        }
+        gameOver(win);
         noLoop();
     }
 }
